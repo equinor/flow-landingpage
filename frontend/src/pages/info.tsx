d8e75cd28c0a8f63a884982b192d8816f7105bc6
Frode Skjæveland <frode.skj@gmail.com>
2022-05-12 19:13:56 +0200

    fixing page
    fixing page

---
+++ frontend/src/pages/info.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from 'react-router-dom';
+import { Button } from '@equinor/eds-core-react';
+  
+export default function Home() {
+    return (
+        <div>
+          <h2>Welcome to FLOW</h2>
+        
+        </div>
+      );
+}
